f572b70d28c3c85992b3977f4e53a8ccabdf3e05
oscar <oscarljnos@gmail.com>
2016-09-22 09:43:12 -0300

    Adding role to devise Admin Employee User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+
+	private
+		def check_admin!
+			authenticate_user!
+		unless current_user.admin?
+			redirect_to root_path, alert: "No tienes acceso"
+		end
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160922120806) do
+ActiveRecord::Schema.define(version: 20160922123304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 20160922120806) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "role",                   default: 2,  null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
